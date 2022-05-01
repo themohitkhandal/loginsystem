675d8cd28fe3c8f186884f1832b8a54168ebe137
Mohit Windows <mohit1319khandal@gmail.com>
2022-05-02 05:24:01 +0530

    removed carousal

--- home.php
+++ home.php
@@ -16,27 +16,7 @@
 <body>
   <?php require 'partials/_nav.php'?>
   <h1 class="text-center">Home</h1>
-  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="partials\wallhaven-l38lpl.png" class="d-block w-100" style="height:50%" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="partials\wallhaven-rd3pjw.jpg" class="d-block w-100" style="height:50%" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="partials\wallhaven-rd32x7.png" class="d-block w-100" style="height:50%" alt="...">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+  
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
